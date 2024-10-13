390477252e9e6641d2fd3ca3cd22e4bcdbb4edaa
Imran Ali <imranali.code@gmail.com>
2024-10-13 16:16:41 +0530

    setup redux Store

--- README.md
+++ README.md
@@ -10,6 +10,10 @@
 - Create BrowserRouter > Routes > Route=/ Body >RouteChildren
 - Create an Outlet in your Body Component
 - Create a footer
+- Create a login page
+- Install axios
+- CORS - install cors in backend => add middleware to with configuration: origin, credentials: true
+- Whenever you're making API call so pass axios=> {withcredentials: true}
 
 Body
 NavBar
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import Profile from "./Profile";
-import Body from "./Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Body from "./components/Body";
+import Feed from "./components/Feed";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/feed" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }

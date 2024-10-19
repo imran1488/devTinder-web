d0b16b44189458642f9ad128f43041e83bfe5a68
Imran Ali <imranali.code@gmail.com>
2024-10-20 03:55:31 +0530

    building feature to edit the profile

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import axios from "axios";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const feed = useSelector((store) => store.feed);
+  console.log(feed);
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    //if (feed) return;
+    try {
+      console.log("testing in feed");
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      console.log(feed);
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
